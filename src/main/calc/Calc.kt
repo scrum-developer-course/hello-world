70964daed40f7eb695e4d3bb3bb71c54e7ec55a7
Lukáš Henzelín (cen89113) <lhenzelin@csas.cz>
2023-04-04 16:29:02 +0200

    calc test cases and code

--- src/main/calc/Calc.kt
+++ src/main/calc/Calc.kt
@@ -1,4 +1,32 @@
 package main.calc
 
 class Calc {
+    fun add(first: String, second: String): String {
+        val result = convert(first) + convert(second)
+        if (result == "IIII") return "IV"
+        if (result == "IIIII") return "V"
+        if (result == "IIIIII") return "VI"
+        if (result == "IIIIIII") return "VII"
+        if (result == "IIIIIIII") return "VIII"
+        if (result == "IIIIIIIII") return "IX"
+        if (result == "IIIIIIIIII") return "X"
+        return result
+    }
+
+    private fun convert(string: String): String {
+        return when (string) {
+            "IV" -> "IIII"
+            "V" -> "IIIII"
+            else -> string
+        }
+    }
+
+    fun add2(first: String, second: String): String {
+        var result = convert(first) + convert(second)
+        result = result.replace("^IIIIIIIIII", "X")
+        result = result.replace("^IIIIIIIII", "IX")
+        result = result.replace("^IIIII", "V")
+        result = result.replace("^IIII", "IV")
+        return result
+    }
 }
--- src/test/calc/CalcTest.kt
+++ src/test/calc/CalcTest.kt
@@ -1,4 +1,156 @@
 package calc
 
+import main.calc.Calc
+import org.testng.annotations.Test
+
 class CalcTest {
+
+    @Test
+    fun firstTest() {
+        val calc = Calc()
+        val result = calc.add("I", "I")
+        assert(result == "II")
+    }
+
+    @Test
+    fun secondTest() {
+        val calc = Calc()
+        val result = calc.add("I", "II")
+        assert(result == "III")
+    }
+
+    @Test
+    fun thirdTest() {
+        val calc = Calc()
+        val result = calc.add("II", "I")
+        assert(result == "III")
+    }
+
+    @Test
+    fun fourthTest() {
+        val calc = Calc()
+        val result = calc.add("I", "III")
+        assert(result == "IV")
+    }
+
+    @Test
+    fun fifthTest() {
+        val calc = Calc()
+        val result = calc.add("II", "II")
+        assert(result == "IV")
+    }
+
+    @Test
+    fun sixthTest() {
+        val calc = Calc()
+        val result = calc.add("III", "I")
+        assert(result == "IV")
+    }
+
+    @Test
+    fun seventhTest() {
+        val calc = Calc()
+        val result = calc.add("II", "III")
+        assert(result == "V")
+    }
+
+    @Test
+    fun eigththTest() {
+        val calc = Calc()
+        val result = calc.add("III", "II")
+        assert(result == "V")
+    }
+    @Test
+    fun test10() {
+        val calc = Calc()
+        val result = calc.add("I", "IV")
+        assert(result == "V")
+    }
+
+    @Test
+    fun test11() {
+        val calc = Calc()
+        val result = calc.add("IV", "I")
+        assert(result == "V")
+    }
+
+    @Test
+    fun test12() {
+        val calc = Calc()
+        val result = calc.add("I", "V")
+        assert(result == "VI")
+    }
+
+    @Test
+    fun test13() {
+        val calc = Calc()
+        val result = calc.add("V", "I")
+        assert(result == "VI")
+    }
+
+    @Test
+    fun test14() {
+        val calc = Calc()
+        val result = calc.add("II", "IV")
+        assert(result == "VI")
+    }
+
+    @Test
+    fun test15() {
+        val calc = Calc()
+        val result = calc.add("III", "III")
+        assert(result == "VI")
+    }
+
+    @Test
+    fun test16() {
+        val calc = Calc()
+        val result = calc.add("IV", "II")
+        assert(result == "VI")
+    }
+
+    @Test
+    fun test17() {
+        val calc = Calc()
+        val result = calc.add("V", "I")
+        assert(result == "VI")
+    }
+
+    @Test
+    fun test18() {
+        val calc = Calc()
+        val result = calc.add("II", "V")
+        assert(result == "VII")
+    }
+    @Test
+    fun test19() {
+        val calc = Calc()
+        val result = calc.add("IV", "IV")
+        assert(result == "VIII")
+    }
+    @Test
+    fun test20() {
+        val calc = Calc()
+        val result = calc.add("IV", "III")
+        assert(result == "VII")
+    }
+    @Test
+    fun test21() {
+        val calc = Calc()
+        val result = calc.add("IV", "V")
+        assert(result == "IX")
+    }
+    @Test
+    fun test22() {
+        val calc = Calc()
+        val result = calc.add("V", "IV")
+        assert(result == "IX")
+    }
+    @Test
+    fun test23() {
+        val calc = Calc()
+        val result = calc.add("V", "V")
+        assert(result == "X")
+    }
+
 }
